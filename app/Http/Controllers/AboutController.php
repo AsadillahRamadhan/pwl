fe4fc1e2e7884168904dafffc6ed1a80e56da428
MuhammadAsadillahRamadhan <curutcepat@gmail.com>
2023-02-22 14:45:44 +0700

    Pertemuan 2 Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    function index(){
+        echo "Nama: Muhammad Asadillah Ramadhan <br> NIM: 2141720093";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,17 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-echo "Selamat Datang";
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', function(){
-    echo "Nama: Muhammad Asadillah Ramadhan<br>";
-    echo "NIM: 2141720093";
-});
+Route::get('/about', [AboutController::class, 'index']);
 
 Route::pattern('id', '[0-9]+');
 
-Route::get('/articles/{id}', function($id){
-    echo "Halaman Artikel dengan ID $id"; 
-});
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
